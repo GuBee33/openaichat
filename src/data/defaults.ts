634b4b94838c54e04c356f0b377646529f762f47
GuBee33 <74045047+GuBee33@users.noreply.github.com>
2024-03-27 17:04:56 +0100

    small update

--- src/data/defaults.ts
+++ src/data/defaults.ts
@@ -1,8 +1,6 @@
-import {
-    SystemMessage,
-} from '../models/Interfaces'
+import { SystemMessage } from '../models/Interfaces'
 // @ts-ignore
-import {ChatCompletionCreateParamsBase} from 'openai'
+import { ChatCompletionCreateParamsBase } from 'openai'
 
 import { availableFunctions } from '../utils/functions'
 
@@ -17,7 +15,7 @@ export const DEFAULT_SYSTEM_MESSAGES: readonly SystemMessage[] = [
                 content: 'you are a developer',
             },
             {
-                content: `I am an Azure OpenAI ChatGPT, designed to emulate a developer.`,
+                content: `I am an OpenAI ChatGPT, designed to emulate a developer.`,
                 role: 'assistant',
                 showmd: true,
             },
@@ -31,7 +29,7 @@ export const DEFAULT_SYSTEM_MESSAGES: readonly SystemMessage[] = [
                 content: 'you are an engineer',
             },
             {
-                content: `I am an Azure OpenAI ChatGPT, designed to emulate an engineer.`,
+                content: `I am an OpenAI ChatGPT, designed to emulate an engineer.`,
                 role: 'assistant',
                 showmd: true,
             },
@@ -45,7 +43,7 @@ export const DEFAULT_SYSTEM_MESSAGES: readonly SystemMessage[] = [
                 content: 'you are a project manager',
             },
             {
-                content: `I am an Azure OpenAI ChatGPT, designed to emulate a project manager.`,
+                content: `I am an OpenAI ChatGPT, designed to emulate a project manager.`,
                 role: 'assistant',
                 showmd: true,
             },
@@ -59,52 +57,13 @@ export const DEFAULT_SYSTEM_MESSAGES: readonly SystemMessage[] = [
                 content: 'you are an assistant',
             },
             {
-                content: `I am an Azure OpenAI ChatGPT, designed to emulate an assistant.`,
-                role: 'assistant',
-                showmd: true,
-            },
-        ],
-    },
-    {
-        name: 'Cpp converter',
-        code: [
-            {
-                role: 'system',
-                content:
-                    'You will convert the given cpp code to C# with the following rules. Remove the m_ at the start of the variable names! Start the method name with a uppercase letter! Instead of getter and setter calls, handle it as a property with the same name!  Use expression body for this and use it in future conversions also if the body is only 1 line! Use switch expression whenever possible!',
-            },
-            {
-                content: `I am an Azure OpenAI ChatGPT, designed to emulate a developer. I will convert the cpp code you provide to C#.`,
+                content: `I am an OpenAI ChatGPT, designed to emulate an assistant.`,
                 role: 'assistant',
                 showmd: true,
             },
         ],
     },
 ]
-
-export const AVAILABLE_MODELS = [
-    {
-        modelType: 'GPT 3.5 Turbo',
-        modelVersions: [
-            {
-                deployment: 'gpt-35-turbo',
-                version: 'Model version: 0301',
-            },
-            {
-                deployment: 'gpt-35-turbo-16k',
-                version: 'Model version: 16k 0613',
-            },
-        ],
-    },
-    {
-        modelType: 'GPT 4',
-        modelVersions: [
-            { deployment: 'gpt-4', version: 'Model version: 0314' },
-        ],
-    },
-]
-
-export const GPT3_WITH_FUNCTIONS = 'gpt-35-turbo-16k' as const
 
 export const DEFAULT_OPTIONS: ChatCompletionCreateParamsBase = {
     temperature: 0.5,
